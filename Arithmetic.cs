7df881b4b09fe3e325a776c1426d5538e29eae70
Harsh Mandavkar <harshmandavkar1503@gmail.com>
2026-02-18 17:12:13 +0530

    Created new file Arithmetic.cs with code

---
+++ Arithmetic.cs
@@ -0,0 +1,22 @@
+// // using System;
+
+// Console.Write("Enter first number: ");
+// int num1 = int.Parse(Console.ReadLine());
+
+// Console.Write("Enter second number: ");
+// int num2 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("\nArithmetic Operations:");
+
+// Console.WriteLine("Addition: " + (num1 + num2));
+// Console.WriteLine("Subtraction: " + (num1 - num2));
+// Console.WriteLine("Multiplication: " + (num1 * num2));
+
+// if (num2 != 0)
+// {
+//     Console.WriteLine("Division: " + ((double)num1 / num2));
+// }
+// else
+// {
+//     Console.WriteLine("Division by zero is not allowed.");
+// }
